48426d700565e2cceafbc3955d4f0cf65abc43b9
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-11-16 14:46:33 -0700

    pullall ran

--- Assets/Scripts/Player/Ship.cs
+++ Assets/Scripts/Player/Ship.cs
@@ -1,16 +1,20 @@
 using System;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShip : MonoBehaviour
 {
     new Camera camera;
+    Vector3 position;
     public PlayerData playerData;
+    public PlayerInput input;
 
     void Start()
     {
         camera = Camera.main;
         playerData.position = gameObject.GetComponent<Transform>().position;
+        input = gameObject.GetComponent<PlayerInput>();
     }
 
     void Update()
@@ -18,7 +22,6 @@ public class PlayerShip : MonoBehaviour
         playerData.position = transform.position;
         camera.transform.position = new (transform.position.x, transform.position.y, -10.0f);
     }
-
     void FixedUpdate()
     {
         Debug.Log("FixedUpdate Updating");
@@ -27,4 +30,6 @@ public class PlayerShip : MonoBehaviour
             transform.position.y + 0.03f
         );
     }
+    void OnSkip()
+    {}
 }
