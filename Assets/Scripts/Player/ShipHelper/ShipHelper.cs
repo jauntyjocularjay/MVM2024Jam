dc5f987803e1ecbde2e65dcd173d8abf5029ccbd
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-11-18 20:25:33 -0700

    made the helper ship generic so other can be slotted in

--- Assets/Scripts/Player/ShipHelper/ShipHelper.cs
+++ Assets/Scripts/Player/ShipHelper/ShipHelper.cs
@@ -1,18 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-abstract public class HelperShip : MonoBehaviour
+public class HelperShip : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    abstract public void Ability();
-
+    public HelperShipData helperShipsData;
 }
--- Assets/Scripts/Player/ShipHelper/SkipDriveShip.cs
+++ Assets/Scripts/Player/ShipHelper/SkipDriveShip.cs
@@ -1,30 +1,19 @@
 using UnityEngine;
 
-public class SkipDriveShip : MonoBehaviour
+[CreateAssetMenu(fileName = "Data", menuName = "Data/Ship/SkipDrive", order = 0)]
+public class HelperShipData : ScriptableObject
 {
     public PlayerData data;
     public float skipDriveDistance = 2.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void Ability()
     /*
      * @method Ability allows the skip drive to instantly teleport to a position as long as there are no
      *      solid objects in the way.
      */
     {
-        transform.position = new Vector2(
-            (data.moveDirection.x * skipDriveDistance) + transform.position.x,
-            (data.moveDirection.y * skipDriveDistance) + transform.position.y
+        data.positionOnMap = new Vector2(
+            (data.moveDirection.x * skipDriveDistance) + data.positionOnMap.x,
+            (data.moveDirection.y * skipDriveDistance) + data.positionOnMap.y
         );
     }
 }
