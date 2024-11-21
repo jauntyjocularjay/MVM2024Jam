e4294e9e93714b0015b30632ae51f1b353438cbb
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-11-21 13:54:26 -0700

    added butterfly sprite

--- Assets/Scripts/Enemy/Soldier/SoldierRed.cs
+++ Assets/Scripts/Enemy/Soldier/SoldierRed.cs
@@ -1,27 +1,35 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RedSoldier : Soldier
 {
-    // SoldierData data;
-    // Sprite sprite;
-    // Path path;
     List<Vector3> waypoints;
+    UFractionScale progress = new UFractionScale(0,512);
+    int p = 0;
+    Vector3 increment;
 
-    // void Start()
-    // {
-    //     sprite = data.sprite;
-    //     path = GetComponent<Path>();
-    // }
     new void Start()
     {
         base.Start();
         waypoints = path.waypoints;
+        p = waypoints.Count;
     }
     
     void Update()
     {
-        foreach(Vector3 point in waypoints)
-        {}
+    }
+    void FixedUpdate() 
+    {
+        UpdatePosition(p, waypoints[p]);
+    }
+    void UpdatePosition(int Progress, Vector3 point)
+    {
+        if(progress.Full())
+        {
+            p++;
+            UpdatePosition(p, waypoints[p]);
+        }
+        progress.Increment();
     }
 }
