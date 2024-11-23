c681e61ed8018708de18cc7e18d0eb8fb85dd130
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-11-23 12:32:52 -0700

    added documentation for HelperShip

--- Assets/Scripts/Player/ShipHelper/ShipHelperData.cs
+++ Assets/Scripts/Player/ShipHelper/ShipHelperData.cs
@@ -2,6 +2,13 @@ using UnityEditor.Animations;
 using UnityEngine;
 
 abstract public class HelperShipData : ScriptableObject
+/*
+    HelperShipData sprites (by inheritance), animator (by inheritance), and the interface for Ability 
+    to be present in all children.
+
+    The ability has access to the PlayerData that we use to keep track of the player character's
+    position among other things.
+*/
 {
     public PlayerData data;
     public Sprite sprite;
