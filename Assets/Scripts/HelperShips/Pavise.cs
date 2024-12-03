26901ea13cad82e96eef7502d724a4f06b25be21
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-12-03 03:03:14 -0700

    set and saved helper ships

--- Assets/Scripts/HelperShips/Pavise.cs
+++ Assets/Scripts/HelperShips/Pavise.cs
@@ -1,14 +1,16 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pavise : HelperShip
 {
     ShipType shipType = ShipType.Pavise;
+    // private Pathwinder pathwinder;
 
     new void Start()
     {
+        // pathwinder = GetComponent<Pathwinder>();
         base.Start();
         HP = 7;
+        // pathwinder.Go();
     }
 
     
